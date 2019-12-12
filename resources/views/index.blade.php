3515517bafb6f7a3841e24c3e9d1afb267b38648
Venipa <admin@venipa.net>
2019-12-13 00:53:46 +0100

    fixed footer alignment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
 
     <div id="app" class="py-5">
         <app-avatar-fetch></app-avatar-fetch>
-    </div>
-    <div class="footer text-muted my-2 d-flex flex-row justify-content-center align-items-center">
-        <span class="mr-1">2019 &copy;</span><a href="https://venipa.net">venipa.net</a>
+        <div class="footer text-muted my-2 d-flex flex-row justify-content-center align-items-center">
+            <span class="mr-1">2019 &copy;</span><a href="https://venipa.net">venipa.net</a>
+        </div>
     </div>
 
 @endsection
